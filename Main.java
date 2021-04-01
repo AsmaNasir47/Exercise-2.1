505e51cc6a0da03e1dfbf5c715cb2294f8b24976
AsmaNasir47 <81314078+AsmaNasir47@users.noreply.github.com>
2021-04-01 23:08:12 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package exercisecorrection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		classiPhone smartphone = new classiPhone();
+		smartphone.printMakeCall();
+		smartphone.printPhone();
+		smartphone.printPlayGame();
+		
+		classHuawei smartphone1 = new classHuawei();
+		smartphone1.printMakeCall();
+		smartphone1.printPhone();
+		smartphone1.printPlayGame();
+ 
+	}
+
+}
---
+++ classiPhone.java
@@ -0,0 +1,18 @@
+package exercisecorrection;
+
+public class classiPhone {
+
+	String phone = "iPhone"; 
+	String makecall = "make a call";
+	String playgame = "play games";
+		
+		void printPhone() {
+			System.out.println(phone);
+		}
+		void printMakeCall() {
+			System.out.println(makecall);
+		}
+		void printPlayGame() {
+			System.out.println(playgame);
+		}
+}
